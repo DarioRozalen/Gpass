d7b0aad1db32bc340cd705f6658de5f6114a6ebc
alumnos <alumnos@409-10.local>
2018-12-19 13:06:51 +0100

    Lo he intentado

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Firebase\JWT\JWT;
 use App\User;
 
 class RegisterController extends Controller
 {
-   
+
     public function register (Request $request)
     {
         if (!isset($_POST['user']) or !isset($_POST['email']) or !isset($_POST['password'])) 
@@ -21,21 +22,28 @@ class RegisterController extends Controller
 
         if (!empty($user) && !empty($email) && !empty($password))
         {
-            try
+            if(strlen($password) >= 8)
             {
-            $users = new User();
-            $users->name = $user;
-            $users->password = $password;
-            $users->email = $email;
+                try
+                {
+                    $users = new User();
+                    $users->name = $user;
+                    $users->password = $password;
+                    $users->email = $email;
 
-            $users->save();
-        }
-        catch(Exception $e)
-            {
-                return $this->error(2, $e->getMessage());
+                    $users->save();
+                }
+                catch(Exception $e)
+                {
+                    return $this->error(2, $e->getMessage());
+                }
+
+                return $this->error(200, 'Usuario registrado');
             }
-            
-            return $this->error(200, 'Usuario registrado');
+            else
+            {
+                return $this->error(401, 'The password must be bigger than 8 characters');
+            }
         }
         else
         {
@@ -43,3 +51,8 @@ class RegisterController extends Controller
         }
     }  
 }
+
+// if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+//             {
+//                 return $this->error(401, 'No puede haber espacios en los campos');
+//             }
